65d32af1424586b635278409850b243eee453acb
Alfin Albert <alfinalbert16@gmail.com>
2024-11-20 23:04:24 +0530

    change in backend main. port is defined

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,8 @@ import (
 	"bdayappbknd/handlers"
 	"bdayappbknd/managers"
 	"github.com/gin-gonic/gin"
+	"os"
+	
 )
 
 func init() {
@@ -17,5 +19,13 @@ func main() {
 	userManager := managers.NewUserManager()                // Pointer
 	userHandler := handlers.NewUserHandlerFrom(userManager) // Use pointer directly
 	userHandler.RegisterUserApis(router)
-	router.Run() // listen and serve on 0.0.0.0:8080
+	// router.Run() // listen and serve on 0.0.0.0:8080
+	port := os.Getenv("PORT") // Use the PORT environment variable
+
+	if port == "" {
+		port = "8080" // Default port if none is set
+	}
+
+	// Run the server on the specified port
+	router.Run(":" + port)
 }
